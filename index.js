dfe5b3880e8ee96fbefba3247b4e5cbcc3a09bb0
MdRaihan3 <1mdr91012@gmail.com>
2024-04-28 23:10:38 +0600

    updated

--- index.js
+++ index.js
@@ -99,10 +99,9 @@ async function run() {
             res.send(result);
         })
 
-
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment.Now You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -112,9 +111,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Tourist Server is running')
+    res.send('Tourist Home Server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Tourist Server is running on port: ${port}`)
+    console.log(`Tourist   Server is running on port: ${port}`)
 })
