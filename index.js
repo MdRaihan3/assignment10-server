44f1d3c3560f951d438482795b1d50149ced9dec
MdRaihan3 <1mdr91012@gmail.com>
2024-04-29 20:43:09 +0600

    updated vercel json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 
@@ -21,10 +21,26 @@ const client = new MongoClient(uri, {
     }
 });
 
+client
+    .connect()
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// Middleware Connections
+const corsConfig = {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+};
+app.use(cors(corsConfig));
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const myCountriesCollection = client.db('touristDB').collection('countries')
         const mySpotsCollection = client.db('touristDB').collection('myCountrySpots')
@@ -100,20 +116,19 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
-
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Tourist Server is runningggggggggggggggg')
+    res.send('Tourist Server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Tourist Server is running on port: ${port}  hiiiiii`)
+    console.log(`Tourist Server is running on port: ${port}`)
 })
